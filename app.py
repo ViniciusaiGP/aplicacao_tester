3d01c20393982bbcb4c31dd0dffeef9181924f00
Elton Junior <eltonjuniorsantos@hotmail.com>
2024-11-28 14:13:29 -0300

    Mudando os nomes das funções

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from functions.screenshot import take_screenshot_interactive
+from functions.screenshot import screenshot
+from functions.event_pyautogui import EventPyautogui
 
 if __name__ == "__main__":
     print("Iniciando o processo de captura de tela...")
-    take_screenshot_interactive()
+    screenshot()
+    EventPyautogui.execute('moveTo', 100, 100)
     print("Captura de tela concluída!")
